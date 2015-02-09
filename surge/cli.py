b20641bf62a9a7e14bf41e52bc3d7c28f4ebc3c3
Marc Solanas Tarre <marc@solanas.cat>
2015-02-09 14:21:50 -0800

    Fixed bug with ssh. Improved help

--- surge/cli.py
+++ surge/cli.py
@@ -22,7 +22,7 @@ def main(ctx, debug):
 @click.option('--openstack', 'provider', flag_value='openstack')
 @click.pass_context
 def deploy(ctx, filename, name, provider):
-    """Surge command line client"""
+    """Deploy a new pipeline"""
     if (ctx.obj['DEBUG']):
         print("Pipeline filename: " + filename)
         print("Provider: " + provider)
@@ -38,7 +38,7 @@ def deploy(ctx, filename, name, provider):
 @click.argument('name', required=True)
 @click.pass_context
 def provision(ctx, name):
-    """Surge command line client"""
+    """Run provisioning on a pipeline"""
 
     v = surge.VagrantDeployer(name)
     v.provision()
@@ -52,7 +52,7 @@ def provision(ctx, name):
 @click.option('--openstack', 'provider', flag_value='openstack')
 @click.pass_context
 def deploy_template(ctx, template, name, provider):
-    """Surge command line client"""
+    """Deploy a pipeline from a template"""
     if (ctx.obj['DEBUG']):
         print("Pipeline filename: " + filename)
     print("Provider: " + provider)
@@ -74,7 +74,7 @@ def deploy_template(ctx, template, name, provider):
 @click.argument('name', required=True)
 @click.pass_context
 def create_template(ctx, filename, name):
-    """Surge command line client"""
+    """Create a template from a pipeline file"""
     path = os.path.dirname(
         os.path.realpath(__file__)) + '/surge_deployer/templates/' + name
     if not os.path.exists(path):
@@ -87,7 +87,7 @@ def create_template(ctx, filename, name):
 @main.command()
 @click.pass_context
 def list_templates(ctx):
-    """Surge command line client"""
+    """List all available templates"""
     p = os.listdir(
         os.path.dirname(os.path.realpath(__file__)) + '/surge_deployer/templates')
     if len(p) is 0:
@@ -100,7 +100,7 @@ def list_templates(ctx):
 @click.argument('name', required=True)
 @click.pass_context
 def destroy(ctx, name):
-    """Surge command line client"""
+    """Destroy a deployed pipeline"""
     # Creating ansible inventory file
     # create_ansible_inventory(ctx, filename, provider)
     click.echo('Checking pipeline...')
@@ -113,7 +113,7 @@ def destroy(ctx, name):
 @click.argument('name', required=True)
 @click.pass_context
 def status(ctx, name):
-    """Surge command line client"""
+    """Check status of a pipeline"""
     click.echo('Checking status...')
 
     v = surge.VagrantDeployer(name)
@@ -123,7 +123,7 @@ def status(ctx, name):
 @main.command()
 @click.pass_context
 def list(ctx):
-    """Surge command line client"""
+    """List currently deployed pipelines"""
     p = os.listdir(
         os.path.dirname(os.path.realpath(__file__)) + '/surge_deployer/pipelines')
     if len(p) is 0:
@@ -137,15 +137,20 @@ def list(ctx):
 @click.argument('vmname', required=True)
 @click.pass_context
 def ssh(ctx, name, vmname):
-    """Surge command line client"""
+    """SSH into an instance"""
     click.echo('Checking status...')
     v = surge.VagrantDeployer(name)
     a = v.ssh(vmname).split("\n")
     a = map(lambda x: x.strip(), a)
-    hostname = a[2].split(' ')[1]
-    user = a[3].split(' ')[1]
-    port = a[4].split(' ')[1]
-    key = a[8].split(' ')[1]
+    data = {}
+    for i in a:
+        e = i.split(" ")
+        if len(e) == 2:
+            data[e[0]] = e[1]
+    hostname = data["HostName"]
+    user = data['User']
+    port = data['Port']
+    key = data['IdentityFile']
     os.system('ssh ' + user + '@' + hostname + ' -p' + port +
               ' -i ' + key + ' -o StrictHostKeyChecking=no')
 
