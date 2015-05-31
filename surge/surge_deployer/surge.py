0422901f19b1ea1efecdc73f2584ee72067536e2
Komei Shimamura <komei.t.f@gmail.com>
2015-05-31 13:34:51 -0700

    Fix pep8 test error

--- surge/surge_deployer/surge.py
+++ surge/surge_deployer/surge.py
@@ -1,5 +1,3 @@
-
-
 # Copyright 2015 Cisco Systems, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
--- tools/send_test_data_to_kafka.py
+++ tools/send_test_data_to_kafka.py
@@ -1,5 +1,3 @@
-
-
 # Copyright 2015 Cisco Systems, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -19,37 +17,36 @@ from kafka import SimpleProducer
 from kafka import KafkaClient 
 from kafka import KafkaConsumer
 
+
 class Producer(object):
 
-	def __init__(self, broker_ip_port):
-		self.kafka = KafkaClient(broker_ip_port)	
-       		self.producer = SimpleProducer(self.kafka)
-	
-	def send_message(self):
-		 response = self.producer.send_messages("HEYA","Hello World","Kafka Deployment Worked!")
-		 return [("Error ",response[0].error ), response ]
+    def __init__(self, broker_ip_port):
+        self.kafka = KafkaClient(broker_ip_port)
+        self.producer = SimpleProducer(self.kafka)
 
-        def close(self):
-		self.kafka.close()
+    def send_message(self):
+        response = self.producer.send_messages("HEY", "Hello World", "Kafka Deployment Worked!")
+        return [("Error ", response[0].error), response]
+
+    def close(self):
+        self.kafka.close()
+
 
 class Consumer(object):
 
-	def __init__(self,broker_ip_port):
-		self.consumer = KafkaConsumer("HEYA", group_id="my_group",
-                         metadata_broker_list=[broker_ip_port])
-	
-	def consume_message(self):
-		print self.consumer
-		for message in self.consumer :
-			print message 
-		print self.consumer
-      
+    def __init__(self,broker_ip_port):
+        self.consumer = KafkaConsumer(
+            "HEYA", group_id="my_group", metadata_broker_list=[broker_ip_port])
 
+    def consume_message(self):
+        print self.consumer
+        for message in self.consumer :
+            print message
+        print self.consumer
 
-if __name__== "__main__":
-	producer = Producer("172.17.1.137:9092")
-	print producer.send_message()
-        producer.close()
-	con = Consumer("172.17.1.137:9092")
-	con.consume_message()
-	
+if __name__ == "__main__":
+    producer = Producer("172.17.1.137:9092")
+    print producer.send_message()
+    producer.close()
+    con = Consumer("172.17.1.137:9092")
+    con.consume_message()
